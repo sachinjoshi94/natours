bfeafd8b4a5f5795d3ca12abd611f3477e200180
Sachin Joshi <sachinjoshi3491@gmail.com>
2023-08-06 00:00:42 +0530

    fix typo in port env variable

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose
     console.log('DB connected');
   });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
